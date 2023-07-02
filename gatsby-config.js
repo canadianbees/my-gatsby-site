a45448713c724fe919a92232325febf515663013
canadianbees <calzenor@outlook.com>
2023-07-02 13:03:47 -0400

    add env

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,14 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: ["gatsby-plugin-styled-components"],
 };
