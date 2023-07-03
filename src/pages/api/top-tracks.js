a5d9e5339ad290a11093d3206ffffae45a063acb
canadianbees <calzenor@outlook.com>
2023-07-02 23:16:13 -0400

    added filtering to tracks

--- src/pages/api/top-tracks.js
+++ src/pages/api/top-tracks.js
@@ -3,13 +3,28 @@ import { getTopTracks } from '../../../lib/spotify.js';
 
 export default async function getMyTopTracks(req, res) {
     const response = await getTopTracks();
-
     const { items } = await response.json();
-    const tracks = items.slice(0, 10).map((track) => ({
+
+    //filter through the list and check if each track has necessary requirements
+    var tracksList = items.filter(function (track) {
+
+        return track.preview_url !== null && 
+        track.artists !== null && 
+        track.name !== null && 
+        track.album.images !== null;
+
+
+    })
+
+
+    //get the first 10 tracks from the filtered list
+    const tracks = tracksList.slice(0, 10).map((track) => ({
+
         artist: track.artists.map((_artist) => _artist.name).join(', '),
         songUrl: track.preview_url,
         title: track.name,
         img: track.album.images[0],
+
     }));
 
     return tracks;
