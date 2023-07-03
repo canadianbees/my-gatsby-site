7caf32255c173b12651a095aad8d680300eb7108
canadianbees <calzenor@outlook.com>
2023-07-02 23:32:28 -0400

    make sure we get data before filtering

--- src/pages/api/top-tracks.js
+++ src/pages/api/top-tracks.js
@@ -6,18 +6,20 @@ export default async function getMyTopTracks(req, res) {
     const { items } = await response.json();
 
     //filter through the list and check if each track has necessary requirements
-    var tracksList = items.filter(function (track) {
 
-        return track.preview_url !== null && 
-        track.artists !== null && 
-        track.name !== null && 
-        track.album.images !== null;
+    if(items)
+    {
+        var tracksList = items.filter(function (track) {
 
+            return track.preview_url !== null && 
+            track.artists !== null && 
+            track.name !== null && 
+            track.album.images !== null;
+    
+    
+        })
 
-    })
-
-
-    //get the first 10 tracks from the filtered list
+        //get the first 10 tracks from the filtered list
     const tracks = tracksList.slice(0, 10).map((track) => ({
 
         artist: track.artists.map((_artist) => _artist.name).join(', '),
@@ -26,6 +28,12 @@ export default async function getMyTopTracks(req, res) {
         img: track.album.images[0],
 
     }));
+    
 
     return tracks;
+    }
+    
+
+    return null;
+    
 };
