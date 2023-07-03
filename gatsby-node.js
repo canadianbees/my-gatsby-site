f709f006555c6de10682083d8de5b47d41c9aa40
canadianbees <calzenor@outlook.com>
2023-07-02 23:23:35 -0400

    polyfill webpack5 shenanigans

---
+++ gatsby-node.js
@@ -0,0 +1,17 @@
+exports.onCreateWebpackConfig = ({ actions }) => {
+    actions.setWebpackConfig({
+      resolve: {
+        fallback: {
+          crypto: require.resolve('crypto-browserify'),
+          path: require.resolve("path-browserify"),
+          os: require.resolve("os-browserify"),
+          stream: require.resolve("stream-browserify"),
+        },
+      },
+      plugins: [
+        new webpack.ProvidePlugin({
+               process: 'process/browser',
+        }),
+      ],
+    })
+  }
