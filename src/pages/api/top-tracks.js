f87013a132a03bdbd41cc7788258eb07ed54c9ba
canadianbees <calzenor@outlook.com>
2023-07-02 23:39:36 -0400

    pesky slice function

--- src/pages/api/top-tracks.js
+++ src/pages/api/top-tracks.js
@@ -4,12 +4,13 @@ import { getTopTracks } from '../../../lib/spotify.js';
 export default async function getMyTopTracks(req, res) {
     const response = await getTopTracks();
     const { items } = await response.json();
+    var tracksList;
 
     //filter through the list and check if each track has necessary requirements
 
     if(items)
     {
-        var tracksList = items.filter(function (track) {
+        tracksList = items.filter(function (track) {
 
             return track.preview_url !== null && 
             track.artists !== null && 
@@ -19,21 +20,21 @@ export default async function getMyTopTracks(req, res) {
     
         })
 
-        //get the first 10 tracks from the filtered list
-    const tracks = tracksList.slice(0, 10).map((track) => ({
+        if(tracksList)
+        {
+             //get the first 10 tracks from the filtered list
+            const tracks = tracksList.slice(0, 10).map((track) => ({
 
-        artist: track.artists.map((_artist) => _artist.name).join(', '),
-        songUrl: track.preview_url,
-        title: track.name,
-        img: track.album.images[0],
+                artist: track.artists.map((_artist) => _artist.name).join(', '),
+                songUrl: track.preview_url,
+                title: track.name,
+                img: track.album.images[0],
+        
+            }));
 
-    }));
-    
-
-    return tracks;
-    }
-    
-
+            return tracks;
+        }
+       
+    };
     return null;
-    
 };
