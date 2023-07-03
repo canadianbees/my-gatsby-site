3c8f3153715127db8846640e2f10a47cc83c32a6
canadianbees <calzenor@outlook.com>
2023-07-03 15:11:02 -0400

    playing local file case

--- src/api/currently-playing.js
+++ src/api/currently-playing.js
@@ -8,13 +8,27 @@ export default async function GetNowPlaying(req,res) {
         return null;
     }
 
+
+   //TODO GET IMAGE INCASE SPOTIFY IMAGE CANT LOAD
  
     const song = await response.json();
+    let image;
+
+    if(song.item.album.images[0])
+    {
+        image = song.item.album.images[0].url;
+    }
+
+    else
+    {
+        image=null;
+    }
+
    
     const result = {
         name: song.item.name,
         trackUrl: song.item.uri,
-        image: song.item.album.images[0].url,
+        image: image,
         isPlaying: song.is_playing,
         artist: song.item.artists[0].name
     };
