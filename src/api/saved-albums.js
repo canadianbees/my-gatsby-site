da641932e5969d3ce6438fcdfbddcc28198b3e16
canadianbees <calzenor@outlook.com>
2023-07-03 00:18:47 -0400

    amove api folder

---
+++ src/api/saved-albums.js
@@ -0,0 +1,17 @@
+import { getSavedAlbums } from "../../lib/spotify";
+
+export default async function GetMySavedAlbums (req,res) {
+    const response = await getSavedAlbums();
+    const { items } = await response.json();
+
+    const result = items.slice(0, 10).map((catalog) => ({
+        name: catalog.album.name,
+        tracks: catalog.album.total_tracks,
+        image: catalog.album.images[0].url,
+        artist: catalog.album.artists[0].name,
+        url: catalog.album.uri
+    }));
+
+    
+    return result;
+}
