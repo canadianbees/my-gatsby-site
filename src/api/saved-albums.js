8e8d998c2d835f4bab9005bc894a6538deb17272
canadianbees <calzenor@outlook.com>
2023-07-03 00:35:13 -0400

    please let this be it

--- src/api/saved-albums.js
+++ src/api/saved-albums.js
@@ -3,7 +3,8 @@ import { getSavedAlbums } from "../../lib/spotify";
 export default async function GetMySavedAlbums (req,res) {
     const response = await getSavedAlbums();
     const { items } = await response.json();
-
+if(items)
+{
     const result = items.slice(0, 10).map((catalog) => ({
         name: catalog.album.name,
         tracks: catalog.album.total_tracks,
@@ -14,4 +15,6 @@ export default async function GetMySavedAlbums (req,res) {
 
     
     return result;
+}
+    return null;
 }
