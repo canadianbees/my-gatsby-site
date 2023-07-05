c54f2f674cf3a97f445a31bd4dfe35c46c5c7725
canadianbees <calzenor@outlook.com>
2023-07-04 22:50:19 -0400

    clean imports,  cosmetics, change struct  of func

--- src/components/nowPlayingDisplay.js
+++ src/components/nowPlayingDisplay.js
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
 import React from 'react';
-import { useState, useEffect } from 'react';
-import GetNowPlaying from '../api/currently-playing';
 import PlayingAnimation from './PlayingAnimation';
 import { Spotify } from '@styled-icons/fa-brands';
 import { MusicNoteBeamed } from '@styled-icons/bootstrap';
@@ -9,15 +7,14 @@ import { keyframes } from 'styled-components';
 
 const PlayingCard = styled.div
     `    
-    transform:translatex(706px) translatey(-10px);
     display:inline-block;
+    background-color: #C3B59F;
     width:467px;
-    height:323px;
     min-height:357px;
+    box-shadow:0px 0px 15px 15px #756d5f;
     border-radius:18px;
-    box-shadow:0px 0px 25px 5px #c1bdbd;
-    a
-    {
+    position: relative;
+    a{
         color:#191414;
     }
 `;
@@ -26,9 +23,12 @@ const SpotifyLogo = styled(Spotify)
     `
     width: 30px;
     height: 30px;
-    transform:translatex(423px) translatey(-67px);
-    &:hover
-    {
+    position:absolute;
+    bottom:0;
+    right:0;
+    padding-right:20px;
+    padding-bottom: 15px;
+    &:hover{
         color: #1DB954;
     }
 `;
@@ -38,7 +38,9 @@ const Listening = styled.span
     font-size:30px;
     font-weight:600;
     display:inline-block;
-    transform:translatex(59px) translatey(-152px);
+    position:relative;
+    top:-157px;
+    left:53px;
 `;
 
 const Offline = styled.span
@@ -51,7 +53,7 @@ const Offline = styled.span
 
 const Animation = styled.div
     `
-transform:translatex(267px) translatey(-109px);
+    transform:translatex(267px) translatey(-109px);
 `;
 
 const PlayingImg = styled.img
@@ -59,11 +61,11 @@ const PlayingImg = styled.img
     width: 200px;
     height: 200px;
     display:inline-block;
- transform:translatex(22px) translatey(48px);
- border-radius:18px;
- border-color:#1DB954;
- border-width:2px;
- border-style:solid;
+    transform:translatex(22px) translatey(48px);
+    border-radius:18px;
+    border-color:#1DB954;
+    border-width:3px;
+    border-style:solid;
 
 `;
 
@@ -71,7 +73,6 @@ const PlayingInfo = styled.span
     `  
     font-size:20px;
     display:block;
-    transform:translatex(17px) translatey(-45px);
     font-weight:600;
     position:relative;
     top:8px;
@@ -92,79 +93,75 @@ const Loading = styled(MusicNoteBeamed)
  height:100px;
  width:100px;
  text-shadow: 0 0 8px #FF0000;
- bottom:-200px;
- top:400px;
- right:400px;
- left:952px;
  position:relative;
- text-align:right;
- transform:translatex(-764px) translatey(-278px);
 
 `;
 
-const NowPlayingDisplay = () => {
+const Bottom = styled.div
+`
+display:flex;
+padding-right:32px;
+min-height:53px;
+width:410px;
+position:relative;
+top:-54px;
+left:22px;
 
-    const [loaded, setLoading] = useState(false);
-    const [nowPlaying, setNowPlaying] = useState();
+`;
 
-    async function fetchData() {
-
-        let playingResult = await GetNowPlaying();
-        setNowPlaying(playingResult);
-        setLoading(true);
-
-        console.log(nowPlaying);
+const NowPlayingDisplay = ({name, artist, image, trackUrl, isPlaying, loaded, online}) => {
+    const LoadingFormat = 
+    {
+        display:'flex',
+        alignItems:'center',
+        flexDirection:'column',
+        justifyContent:'center',
     }
 
-    useEffect(() => {
-        fetchData();
-
-    }, []); //! Important
-
     if (loaded) {
-        if (nowPlaying) {
+        if (online) {
 
 
             return (
 
                 <>
+                    {/* if im online and playing anything it will display it, if im just online but the music is paused it will display not playing anything right now */}
+                    {isPlaying ?
 
-                    {nowPlaying.isPlaying ?
 
-
-                        <>
+                   
                             <PlayingCard>
-                                <PlayingImg src={nowPlaying.image} alt="" />
+                                <PlayingImg src={image} alt="" />
                                 <Listening>Now Playing</Listening>
-                                <Animation>{nowPlaying.isPlaying && <PlayingAnimation />} </Animation>
-                                <PlayingInfo>{nowPlaying.name}<br></br>{nowPlaying.artist}</PlayingInfo>
-                                <a href={nowPlaying.trackUrl}><SpotifyLogo></SpotifyLogo></a>
+                                <Animation>{isPlaying && <PlayingAnimation />} </Animation>
+                                <Bottom>
+                                <PlayingInfo>{name}<br></br>{artist}</PlayingInfo>
+                                </Bottom>
+                                <a href={trackUrl} aria-label="Open song" title="Interested? Click to open the song in Spotify!"><SpotifyLogo></SpotifyLogo></a>
                             </PlayingCard>
 
-                        </>
+                      
 
 
-                        : <Offline>Not playing anything at the moment</Offline>}
+                        : <Offline>Not playing anything at the moment<br></br><br></br></Offline>}
 
 
                 </>
 
             )
         }
-
+        // im not online
         else {
-            return (<Offline>Currently offline</Offline>)
+            return (<Offline>Currently offline<br></br><br></br></Offline>)
         }
 
     }
 
+    //display loading animation if the player needs time to laod
     else {
-        return (<PlayingCard>
-            
+        return (<PlayingCard style={LoadingFormat}>
             <Loading />
-            <PlayingInfo style={{color:'white',  transform:'translate(4px,174px)', cursor:'default'}}>BLAAAAAAAAAAAAAAAAAAAAAAAH<br></br>BLAAAAAAAAAAAAAAAAAAAAAAAH</PlayingInfo>
-            <SpotifyLogo style={{color:'white',  transform:'translate(423px,-67px)'}} ></SpotifyLogo>
-
+            <SpotifyLogo style={{color:'#C3B59F'}} ></SpotifyLogo>
         </PlayingCard>
         )
     }
