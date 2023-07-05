cb8bd2052e9a92418d5b85947488393e280ff5a0
canadianbees <calzenor@outlook.com>
2023-07-04 22:52:07 -0400

    clean imports, remove child element

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,9 @@
 import * as React from "react";
-import styled from "styled-components";
-import { Link } from 'gatsby';
 import Layout from '../components/layout'
-
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
     </Layout>
   )
 }
