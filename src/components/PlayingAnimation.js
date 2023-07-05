c818ba43540822f9d5a93d79b7ad080b750bb3a4
canadianbees <calzenor@outlook.com>
2023-07-04 22:51:39 -0400

    edit position and animation

--- src/components/PlayingAnimation.js
+++ src/components/PlayingAnimation.js
@@ -29,24 +29,24 @@ const Playing = styled.div`
     justify-content: space-between;
     width: 100px;
     height: 100px;
-    transform:translatex(7px) translatey(5px);
+    top:-1px;
+    left:29px;
   
     span {
         width: 25px;
-        height: 100%;
+        height: 150%;
         background-color: #1DB954;
-        box-shadow:8px 0px 9px -3px #229924;
         border-radius: 4px;
-        animation: ${Animation} 4.2s ease infinite alternate;
+        animation: ${Animation} 5.2s ease infinite alternate;
         content: '';
         transform-origin: bottom;
 
         &:nth-of-type(2) {
-            animation-delay: -4.2s;
+            animation-delay: -5.2s;
         }
     
         &:nth-of-type(3) {
-            animation-delay: -4.7s;
+            animation-delay: -5.7s;
         }
         
     }
