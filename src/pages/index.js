77ae2bb38982a2635cb56d232ef0df6aa2902b45
canadianbees <calzenor@outlook.com>
2023-07-10 16:05:06 -0400

    fix linkedin link, added transitions

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,48 +7,48 @@ import { Indeed } from '@styled-icons/simple-icons';
 import { Github } from '@styled-icons/bootstrap'
 
 
-const Logos = styled.div
-  `
+const Logos = styled.div`
   display:flex;
   width:300px;
   justify-content: space-between;
+  
 `;
-const LinkedinLogo = styled(Linkedin)
-  `
+const LinkedinLogo = styled(Linkedin)`
   width: 50px;
   height: 50px;
   color: #090C08;
+  transition: 0.3s;
   &:hover{
     color: #FFB2C0;
   }
 
 `;
 
-const HandshakeLogo = styled(Handshake)
-  `
+const HandshakeLogo = styled(Handshake)`
   width: 50px;
   height: 50px;
   color: #090C08;
+  transition: 0.3s;
   &:hover{
     color: #FFB2C0;
   }
 `;
 
-const IndeedLogo = styled(Indeed)
-  `
+const IndeedLogo = styled(Indeed)`
   width: 50px;
   height: 50px;
   color: #090C08;
+  transition: 0.3s;
   &:hover{
     color: #FFB2C0;
   }
 `;
 
-const GithubLogo = styled(Github)
-  `
+const GithubLogo = styled(Github)`
   width: 50px;
   height: 50px;
   color: #090C08;
+  transition: 0.3s;
   &:hover{
     color: #FFB2C0;
   }
@@ -58,7 +58,7 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <Logos>
-        <a href='www.linkedin.com/in/celina-alzenor-073b7a237'><LinkedinLogo /></a>
+        <a href='https://linkedin.com/in/celina-alzenor-073b7a237'><LinkedinLogo /></a>
         <a href='https://ucf.joinhandshake.com/stu/users/26313927'><HandshakeLogo /></a>
         <a href='https://profile.indeed.com/?hl=en_US&co=US&from=gnav-employer--post-press--jobseeker'><IndeedLogo /></a>
         <a href='https://github.com/canadianbees'><GithubLogo/></a>
